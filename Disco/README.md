99d9f5d1714a8308407a0e415b2b00a1ba8ec80c
Medha Naik <medha.naik@ufl.edu>
2021-07-17 22:57:21 -0700

    Disco - simple app that creates random colors on every tap.

---
+++ Disco/README.md
@@ -0,0 +1,5 @@
+#  Disco
+
+App to learn basic UI interactions in UIKit
+Tap anywhere on the screen to get a different color!
+
--- Disco/ViewController.swift
+++ Disco/ViewController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor {
+    static var random: UIColor {
+        return .init(hue: .random(in: 0...1), saturation: 1, brightness: 1, alpha: .random(in: 0.2...1))
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet var discoButton: UIButton!
+    
+    fileprivate func updateUI() {
+        if discoButton.backgroundColor == .systemBackground {
+            discoButton.backgroundColor = .random
+            discoButton.setTitleColor(.systemBackground, for: .normal)
+        } else {
+            discoButton.backgroundColor = .systemBackground
+            discoButton.setTitleColor(.random, for: .normal)
+        }
+        
+//        var typeface = UIFont.familyNames.randomElement()
+//
+//        print(typeface ?? 0)
+        
+        
+        discoButton.titleLabel!.font =  UIFont(name: "American Typewriter", size: 30)
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        updateUI()
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
 
